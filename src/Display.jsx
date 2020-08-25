292a4610dd3eb7452c0d41aa018ec762ee9d9619
fdavison1 <fred.noreply@gmail.com>
2020-08-25 17:18:36 -0500

    color scheme

--- src/Display.jsx
+++ src/Display.jsx
@@ -3,12 +3,14 @@ import fred from './assets/fred.png';
 
 export const Display = () => {
     return (
+        <div className='flex'>
         <div className='display'>
-            <h1>Fred Davison</h1>
-            <h4>web developer</h4>
             <div className='pink'>
             <img src={fred} alt="" />
             </div>
+            <h1>Fred Davison</h1>
+            <h4>web developer</h4>
+        </div>
         </div>
     )
 }
