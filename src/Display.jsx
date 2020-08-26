c8d38a9f67b3bedb7d1297ddcada0524db1c2987
fdavison1 <fred.noreply@gmail.com>
2020-08-26 15:10:21 -0500

    add tab logo

--- src/Display.jsx
+++ src/Display.jsx
@@ -4,13 +4,13 @@ import fred from './assets/fred.png';
 export const Display = () => {
     return (
         <div className='flex'>
-        <div className='display'>
-            <div className='pink'>
-            <img src={fred} alt="" />
+            <div className='display'>
+                <div className='pink'>
+                    <img src={fred} alt="" />
+                </div>
+                <h1>Fred Davison</h1>
+                <h4>web developer</h4>
             </div>
-            <h1>Fred Davison</h1>
-            <h4>web developer</h4>
-        </div>
         </div>
     )
 }
