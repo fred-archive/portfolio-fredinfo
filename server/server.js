5f9b53015ec72b1bacc2bc1ff7b28d6359b86975
fdavison1 <fred.noreply@gmail.com>
2020-08-26 19:53:48 -0500

    server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path'); 
+require('dotenv').config();
+const express = require('express');
+const { SERVER_PORT } = process.env;
+
+const app = express();
+app.use(express.json());
+
+
+app.use( express.static( `${__dirname}/../build` ) );
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../build/index.html'));
+    });
+
+app.listen(SERVER_PORT, () => console.log(`port ${SERVER_PORT} is on and ready to copy`));
